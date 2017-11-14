b1e97daa907c56805d42e5be12d635e36d98d013
Mark <mamjong@avans.nl>
2017-11-14 16:48:01 +0100

    Section 11 - Implemented result limiting
    Implemented "skip" and "limit" query modifiers to limit the amount of data returned from the database. Useful in large databases. Also used the "sort" modifier to sort the database result.

--- users/test/read_test.js
+++ users/test/read_test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-	let joe;
+	let joe, maria, alex, zach;
 
 	beforeEach((done) => {
 		joe = new User({ name: 'Joe'});
-		joe.save()
+		maria = new User({ name: 'Maria' });
+		alex = new User({ name: 'Alex' });
+		zach = new User({ name: 'Zach' });
+
+		Promise.all([joe.save(), maria.save(), alex.save(), zach.save()])
 			.then(() => done());
 	});
 
@@ -24,5 +28,18 @@ describe('Reading users out of the database', () => {
 			assert(user.name === joe.name);
 			done();
 		});
+	});
+
+	it('can skip and limit the result set', (done) => {
+		User.find({})
+			.sort({ name: 1 })
+			.skip(1)
+			.limit(2)
+			.then((users) => {
+			assert(users.length === 2);
+			assert(users[0].name === 'Joe');
+			assert(users[1].name === 'Maria');
+			done();
+			});
 	});
 });
